8e14ddfc16eb35e57363740598d0e0fb51c88f14
Vedanth <svedanth005@gmail.com>
2024-10-18 14:36:50 +0530

    urls and route

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return "Main page!"
+
+@app.route('/hello')
+def hello():
     return "Hello, World!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello, {name}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3001, debug=True)
